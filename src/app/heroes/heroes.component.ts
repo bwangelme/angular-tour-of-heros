22b93b5d73e9b033bc1a1b1f17b285a5d508cfee
xuyundong <xuyundong@douban.com>
2024-01-11 12:01:34 +0800

    add heros list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { HEROS } from '../mock-heroes';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  standalone: true,
+  styleUrl: './heroes.component.scss',
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+    NgFor,
+  ]
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windsotrm',
+  heroes = HEROS
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
